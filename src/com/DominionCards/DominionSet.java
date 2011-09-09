06f70385b09a1a9d74aecd2d9775b816c5f0cbd5
TheDirtyCalvini <aaron.haser@gmail.com>
2011-09-08 20:33:49 -0400

    Copyright notices

--- src/com/DominionCards/DominionSet.java
+++ src/com/DominionCards/DominionSet.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2011.
+ *
+ *     This file is part of Dominion Randomizer.
+ *
+ *     Dominion Randomizer is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Dominion Randomizer is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Dominion Randomizer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *     Except for stuff that belongs to Donald Vaccarino and Rio Grande Games (basically any text from the game).
+ */
+
 package com.DominionCards;
 
 import android.graphics.drawable.Drawable;
